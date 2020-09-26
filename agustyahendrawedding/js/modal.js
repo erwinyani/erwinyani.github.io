72ee5f919ab8458b2993cc701c834f7be16a89f1
erwinaris <vendettaajah@gmail.com>
2020-09-26 12:09:40 +0700

    update files, rev17 tambah agb12.html

--- agustyahendrawedding/js/modal.js
+++ agustyahendrawedding/js/modal.js
@@ -1,16 +1,21 @@
-// const invited = document.getElementById('modal');
-// invited.innerHTML = '';
 
-$(window).load(function () {
-    $('#myModal').modal('show');
-});
+// $(window).load(function () {
+//         $('#myModal').modal('show');
+//     });
+
+$('#myModal').modal('show');
 
 const inputan = document.querySelector('input');
 const log = document.getElementById('invited');
+const up = document.getElementById('name');
+const set = document.createAttribute('value');
 
 inputan.addEventListener('input', updateValue);
 
-function updateValue(e){
+function updateValue(e) {
     log.textContent = e.target.value;
+
+    set.value = '';
+    up.setAttributeNode(set);
 }
 
