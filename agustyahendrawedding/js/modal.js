fdd3eab5baa26214b2d7a36c57b0c008626a4716
erwinaris <vendettaajah@gmail.com>
2020-09-28 21:13:30 +0700

    update files, rev1 modified modal.js

--- agustyahendrawedding/js/modal.js
+++ agustyahendrawedding/js/modal.js
@@ -1,15 +1,14 @@
-// $(window).load(function () {
-//     $('#myModal').modal('show');
-// });
-
-$('#myModal').modal('show');
+$('#myModal').modal({
+    backdrop: 'static',
+    keyboard: false
+})
 
 const inputan = document.querySelector('input');
 const log = document.getElementById('invited');
 
 inputan.addEventListener('input', updateValue);
 
-function updateValue(e){
+function updateValue(e) {
     log.textContent = e.target.value;
 }
 
